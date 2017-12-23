ded0d3d453a32eb9de7abdc0f627c058ebc0d562
Shane Vitarana <shanev@gmail.com>
2017-12-23 16:34:00 -0500

    Setup Twitter

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var Twitter = require('twitter');
 
 var client = new Twitter({
@@ -12,3 +13,13 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
     console.log(tweets);
   }
 });
+
+// var stream = client.stream('statuses/filter', {track: 'javascript'});
+// stream.on('data', function(event) {
+//   console.log(event && event.text);
+// });
+
+// stream.on('error', function(error) {
+//   // throw error;
+//   console.log(error);
+// });
