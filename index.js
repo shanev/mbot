70dbb367c42f02025081432238a90b6c0ee0fb0e
Shane Vitarana <shanev@gmail.com>
2017-12-23 15:53:46 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  bearer_token: process.env.TWITTER_BEARER_TOKEN
+});
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
