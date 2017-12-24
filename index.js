e50e84d86a7075f5b12623c17d5c1fa5b7560a2f
Shane Vitarana <shanev@gmail.com>
2017-12-23 19:41:23 -0500

    Streaming API

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
 require('dotenv').config();
 var Twitter = require('twitter');
 
+// var client = new Twitter({
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   bearer_token: process.env.TWITTER_BEARER_TOKEN
+// });
+
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  bearer_token: process.env.TWITTER_BEARER_TOKEN
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
+/**
+ * Stream statuses filtered by keyword
+ * number of tweets per second depends on topic popularity
+ **/
+client.stream('statuses/filter', {follow: '961445378'},  function(stream) {
+  stream.on('data', function(tweet) {
+    if (isReply(tweet) == false) {
+      console.log('REAL ' + tweet.text);
+    } else {
+      console.log('REPLY ' + tweet.text);
+    }
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
 });
 
-// var stream = client.stream('statuses/filter', {track: 'javascript'});
-// stream.on('data', function(event) {
-//   console.log(event && event.text);
+// client.get('statuses/user_timeline', {screen_name: 'officialmcafee'}, function(error, tweets, response) {
+//   if (!error) {
+//     for (var i = 0; i < tweets.length ; i++) {
+//       console.log(tweets[i].text);
+//     }
+//   }
 // });
 
-// stream.on('error', function(error) {
-//   // throw error;
-//   console.log(error);
+// does not work
+// client.stream('statuses/user_timeline', {screen_name: 'officialmcafee'}, function(stream) {
+//   stream.on('data', function(tweet) {
+//     console.log(tweet.text);
+//   });
+
+//   stream.on('error', function(error) {
+//     console.log(error);
+//   });
 // });
+
+function isReply(tweet) {
+  if ( tweet.retweeted_status
+    || tweet.in_reply_to_status_id
+    || tweet.in_reply_to_status_id_str
+    || tweet.in_reply_to_user_id
+    || tweet.in_reply_to_user_id_str
+    || tweet.in_reply_to_screen_name )
+    return true
+}
