87e46924f07967ca73647d1dbabed9da196d3446
Shane Vitarana <shanev@gmail.com>
2017-12-23 19:55:17 -0500

    Streaming McAfee tweets

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 require('dotenv').config();
-var Twitter = require('twitter');
 
-// var client = new Twitter({
-//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
-//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//   bearer_token: process.env.TWITTER_BEARER_TOKEN
-// });
+var Twitter = require('twitter');
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -14,10 +9,8 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-/**
- * Stream statuses filtered by keyword
- * number of tweets per second depends on topic popularity
- **/
+ // shanev: 6068692
+ // officialmcafee: 961445378
 client.stream('statuses/filter', {follow: '961445378'},  function(stream) {
   stream.on('data', function(tweet) {
     if (isReply(tweet) == false) {
@@ -32,31 +25,14 @@ client.stream('statuses/filter', {follow: '961445378'},  function(stream) {
   });
 });
 
-// client.get('statuses/user_timeline', {screen_name: 'officialmcafee'}, function(error, tweets, response) {
-//   if (!error) {
-//     for (var i = 0; i < tweets.length ; i++) {
-//       console.log(tweets[i].text);
-//     }
-//   }
-// });
-
-// does not work
-// client.stream('statuses/user_timeline', {screen_name: 'officialmcafee'}, function(stream) {
-//   stream.on('data', function(tweet) {
-//     console.log(tweet.text);
-//   });
-
-//   stream.on('error', function(error) {
-//     console.log(error);
-//   });
-// });
-
 function isReply(tweet) {
   if ( tweet.retweeted_status
     || tweet.in_reply_to_status_id
     || tweet.in_reply_to_status_id_str
     || tweet.in_reply_to_user_id
     || tweet.in_reply_to_user_id_str
-    || tweet.in_reply_to_screen_name )
-    return true
+    || tweet.in_reply_to_screen_name ) {
+    return true;
+  }
+  return false;
 }
