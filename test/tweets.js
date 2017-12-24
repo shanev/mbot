c76ab60be1170443d88e7486a5402d7530309940
Shane Vitarana <shanev@gmail.com>
2017-12-23 21:52:50 -0500

    Tests for coin symbol regex

--- test/tweets.js
+++ test/tweets.js
@@ -1,1 +1,35 @@
-console.log("tests");
+var assert = require('assert');
+
+describe('Tweet', () => {
+  const re = /[A-Z0-9]{3,5}/;
+
+  it('should find DGB symbol in tweet', () => {
+    const tweet = 'Coin of the day: Digibyte (DGB). Using a Blockchain which is 40 times faster than Bitcoin and having one of the most decentralized mining systems in the world - based on 5 different synergistic algorithms. DGB adherents call the coin "The Sleeping Giant".';
+    const symbol = tweet.match(re)[0];
+    assert.equal(symbol, 'DGB');
+  });
+
+  it('should find BURST symbol in tweet', () => {
+    const tweet = 'Coin of the day: BURST -- First truly Green coin and most overlooked coin. Uses 400 times less power than Bitcoin. Super secure and private. Includes smart contracts, encrypted messaging, decentralized wallet, libertine blockchain. Most undervalued coin. https://www.burst-coin.org';
+    const symbol = tweet.match(re)[0];
+    assert.equal(symbol, 'BURST');
+  });
+
+  it('should find 400 symbol in tweet', () => {
+    const tweet = 'Coin of the day: 400 -- First truly Green coin and most overlooked coin. Uses 400 times less power than Bitcoin. Super secure and private. Includes smart contracts, encrypted messaging, decentralized wallet, libertine blockchain. Most undervalued coin. https://www.burst-coin.org';
+    const symbol = tweet.match(re)[0];
+    assert.equal(symbol, '400');
+  });
+
+  it('should not find burst symbol in tweet', () => {
+    const tweet = 'Coin of the day: burst -- First truly Green coin and most overlooked coin. Uses 40 times less power than Bitcoin. Super secure and private. Includes smart contracts, encrypted messaging, decentralized wallet, libertine blockchain. Most undervalued coin. https://www.burst-coin.org';
+    const found = tweet.match(re);
+    assert.equal(found, null);
+  });
+
+  it('should not find symbol in tweet', () => {
+    const tweet = 'This tweet does not contain a symbol.';
+    const found = tweet.match(re);
+    assert.equal(found, null);
+  });
+});
