9be8fa413f8f5f4a7bf05ba9fda46ccd6b93e024
Shane Vitarana <shanev@gmail.com>
2017-12-27 22:44:17 +0530

    Added longer timeout for Coin Market Cap test

--- test/coinmktcap.js
+++ test/coinmktcap.js
@@ -5,10 +5,10 @@ describe('CoinMarketCap API', () => {
   describe('findBySymbol', () => {
     it('should find ticker id for BTC', async () => {
       assert.equal(await CoinMktCapApi.findTickerIdBySymbol('BTC'), 'bitcoin');
-    });
+    }).timeout(5000);
 
     it('should not find ticker id for SHANE', async () => {
       assert.equal(await CoinMktCapApi.findTickerIdBySymbol('SHANE'), null);
-    });
+    }).timeout(5000);
   });
 });
